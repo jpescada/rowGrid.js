c52ac0853c988ca0b4e60d74666e1005e9591462
Joao Pescada <joao@pescada.pt>
2017-04-27 15:30:35 +0100

    Cleaned up code

--- jquery.row-grid.js
+++ jquery.row-grid.js
@@ -12,7 +12,7 @@
         $this.data('grid-options', options);
         layout(this, options);
 
-        if(options.resize) {
+        if (options.resize) {
           $(window).on('resize.rowGrid', {container: this}, function(event) {
             layout(event.data.container, options);
           });
@@ -29,18 +29,19 @@
     firstItemClass: null
   };
 
-  function layout(container, options, items) {
+  function layout(container, options, _items) {
     var rowWidth = 0,
         rowElems = [],
-        items = jQuery.makeArray(items || container.querySelectorAll(options.itemSelector)),
+        items = jQuery.makeArray(_items || container.querySelectorAll(options.itemSelector)),
         itemsSize = items.length;
-    // read
 
+    // read
     var containerBoundingRect = container.getBoundingClientRect();
-    var containerWidth = Math.floor(containerBoundingRect.right - containerBoundingRect.left)-parseFloat($(container).css('padding-left'))-parseFloat($(container).css('padding-right'));
+    var containerWidth = Math.floor(containerBoundingRect.right - containerBoundingRect.left) - parseFloat($(container).css('padding-left')) - parseFloat($(container).css('padding-right'));
     var itemAttrs = [];
     var theImage, w, h;
-    for(var i = 0; i < itemsSize; ++i) {
+
+    for (var i = 0; i < itemsSize; ++i) {
       theImage = items[i].getElementsByTagName('img')[0];
       if (!theImage) {
         items.splice(i, 1);
@@ -48,6 +49,7 @@
         --itemsSize;
         continue;
       }
+
       // get width and height via attribute or js value
       if (!(w = parseInt(theImage.getAttribute('width')))) {
         theImage.setAttribute('width', w = theImage.offsetWidth);
@@ -64,7 +66,9 @@
     itemsSize = items.length;
 
     // write
-    for(var index = 0; index < itemsSize; ++index) {
+    for (var index = 0; index < itemsSize; ++index) {
+      var rowElemIndex;
+
       if (items[index].classList) {
         items[index].classList.remove(options.firstItemClass);
         items[index].classList.remove(options.lastRowClass);
@@ -77,8 +81,8 @@
       rowElems.push(items[index]);
 
       // check if it is the last element
-      if(index === itemsSize - 1) {
-        for(var rowElemIndex = 0; rowElemIndex<rowElems.length; rowElemIndex++) {
+      if (index === itemsSize - 1) {
+        for(rowElemIndex = 0; rowElemIndex<rowElems.length; rowElemIndex++) {
           // if first element in row
           if(rowElemIndex === 0) {
             rowElems[rowElemIndex].className += ' ' + options.lastRowClass;
@@ -90,25 +94,30 @@
       }
 
       // check whether width of row is too high
-      if(rowWidth + options.maxMargin * (rowElems.length - 1) > containerWidth) {
-        var diff = rowWidth + options.maxMargin * (rowElems.length - 1) - containerWidth;
-        var nrOfElems = rowElems.length;
+      if (rowWidth + options.maxMargin * (rowElems.length - 1) > containerWidth) {
+        var diff = rowWidth + options.maxMargin * (rowElems.length - 1) - containerWidth,
+            nrOfElems = rowElems.length,
+            maxSave = (options.maxMargin - options.minMargin) * (nrOfElems - 1),
+            rowMargin;
+
         // change margin
-        var maxSave = (options.maxMargin - options.minMargin) * (nrOfElems - 1);
-        if(maxSave < diff) {
-          var rowMargin = options.minMargin;
+        if (maxSave < diff) {
+          rowMargin = options.minMargin;
           diff -= (options.maxMargin - options.minMargin) * (nrOfElems - 1);
         } else {
-          var rowMargin = options.maxMargin - diff / (nrOfElems - 1);
+          rowMargin = options.maxMargin - diff / (nrOfElems - 1);
           diff = 0;
         }
+
         var rowElem,
-          widthDiff = 0;
-        for(var rowElemIndex = 0; rowElemIndex<rowElems.length; rowElemIndex++) {
+            widthDiff = 0;
+
+        for (rowElemIndex = 0; rowElemIndex<rowElems.length; rowElemIndex++) {
           rowElem = rowElems[rowElemIndex];
           var rowElemWidth = itemAttrs[index+parseInt(rowElemIndex)-rowElems.length+1].width;
           var newWidth = rowElemWidth - (rowElemWidth / rowWidth) * diff;
           var newHeight = Math.round(itemAttrs[index+parseInt(rowElemIndex)-rowElems.length+1].height * (newWidth / rowElemWidth));
+
           if (widthDiff + 1 - newWidth % 1 >= 0.5 ) {
             widthDiff -= newWidth % 1;
             newWidth = Math.floor(newWidth);
@@ -116,15 +125,18 @@
             widthDiff += 1 - newWidth % 1;
             newWidth = Math.ceil(newWidth);
           }
+
           rowElem.style.width = newWidth + 'px';
           rowElem.style.height = newHeight + 'px';
           rowElem.style.marginRight = ((rowElemIndex < rowElems.length - 1)?rowMargin : 0) + 'px';
-          if(rowElemIndex === 0) {
+
+          if (rowElemIndex === 0) {
             rowElem.className += ' ' + options.firstItemClass;
           }
         }
+
         rowElems = [],
-          rowWidth = 0;
+        rowWidth = 0;
       }
     }
   }
